9d50bfd39c9fb1d5adbffab04d72b90571db3266
berrachdis <berrachdis@gmail.com>
2021-02-11 20:58:49 +0100

    feat: init project

---
+++ app/src/test/java/com/myotd/imageprocessor/ImageprocessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myotd.imageprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageprocessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
